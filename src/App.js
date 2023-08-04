0ecada86ac22762f98e9170d37e65e64a0c21d5e
YouBing <2900856508@qq.com>
2023-08-04 21:41:24 +0800

    项目搭建-配置路由

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Home from "./pages/Layout";
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        App
+        <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link>
+        {/* 配置路由的规则 */}
+        <Switch>
+          <Route path="/login" component={Home}></Route>
+          <Route path="/home" component={Login}></Route>
+          {/* 配置一个404组件 */}
+          
+        </Switch>
+      </div>
+    </Router>
+  );
+}
