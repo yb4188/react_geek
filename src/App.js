5eb4b68b6bb4c4d7f2a760ada42d6666c8a9856a
YouBing <2900856508@qq.com>
2023-08-04 23:33:32 +0800

    登录功能基本结构

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-
 import Home from "./pages/Layout";
 import Login from "./pages/Login";
 
 export default function App() {
   return (
     <Router>
+      {/* <Link to="/login">登录</Link>
+      <Link to="/home">首页</Link> */}
       <div>
-        App
-        <Link to="/login">登录</Link>
-        <Link to="/home">首页</Link>
         {/* 配置路由的规则 */}
         <Switch>
           <Route path="/login" component={Login}></Route>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import { Button } from "antd";
+import { Card } from "antd";
+import "./index.css";
+import logo from "../../assets/images/logo.png";
 
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        Login
-        <Button type="primary">test123456</Button>
+      <div className="login-container">
+        <Card className="login" >
+          <img src={logo} alt="" className="login-img"/>
+        </Card>
       </div>
     );
   }
 }
-
